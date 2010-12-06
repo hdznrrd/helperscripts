e3574be99a96588f838f58b28427f5e63165662d
hdznrrd <hadez@nrrd.de>
2010-12-06 22:11:20 +0100

    added cummulative income from membership fees

--- gnuplot/shackspace/cummulatememberscsv.pl
+++ gnuplot/shackspace/cummulatememberscsv.pl
@@ -5,6 +5,7 @@
 
 
 %bydate=();
+%fees=();
 
 $dump = <STDIN>;
 while($_ = <STDIN>)
@@ -14,12 +15,18 @@ while($_ = <STDIN>)
     
     $bydate{$dat[2]} += 1;
     $bydate{$dat[3]} -= 1 if($dat[3] != 0); 
+    $fees{$dat[2]} += $dat[1]==1?8:20;
+    $fees{$dat[3]} -= ($dat[1]==1?8:20) if($dat[3] != 0); 
 }
 
 
 $members = 0;
+$income = 0;
+print "#\tmember count\tmembership fees\n";
+
 foreach(sort keys %bydate)
 {
     $members += $bydate{$_};
-    print "$_ $members\n";
+    $income += $fees{$_};
+    print "$_ $members $income\n";
 }
