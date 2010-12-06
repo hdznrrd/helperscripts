a639782199739cc4098051bf76e96e79ac28e155
hdznrrd <hadez@nrrd.de>
2010-12-06 22:25:25 +0100

    fixed typo

--- gnuplot/shackspace/process.sh
+++ gnuplot/shackspace/process.sh
@@ -2,5 +2,6 @@
 
 cat 101109_liquiditaet_mitgliedsbeitrag_shack.csv | ./liqicsv2memberscsv.pl|./fixliqicsvdates.pl >members.csv
 cat members.csv |./deltamemberscsv.pl >membersdelta.dat
-cat members.csv |./cummulativememberscsv.pl >memberscummulative.dat
+cat members.csv |./cummulatememberscsv.pl >memberscummulative.dat
+cat *txt |./bookings2csv.pl >bookings.dat
 ./replot.sh
