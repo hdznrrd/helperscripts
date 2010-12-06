faa5f03722bd6c2b91f5c9b7ce01527c328a3ec2
hdznrrd <hadez@nrrd.de>
2010-12-06 20:00:49 +0100

    updated script to use replot

--- gnuplot/shackspace/process.sh
+++ gnuplot/shackspace/process.sh
@@ -3,4 +3,4 @@
 cat 101109_liquiditaet_mitgliedsbeitrag_shack.csv | ./liqicsv2memberscsv.pl|./fixliqicsvdates.pl >members.csv
 cat members.csv |./deltamemberscsv.pl >membersdelta.dat
 cat members.csv |./cummulativememberscsv.pl >memberscummulative.dat
-gnuplot members.plot
+./replot.sh
