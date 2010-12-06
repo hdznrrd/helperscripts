cacae6f9c819e9d79d1396a676082978ec7b4ff8
hdznrrd <hadez@nrrd.de>
2010-12-06 19:54:38 +0100

    added toplevel process script that runs all subscripts

---
+++ gnuplot/shackspace/process.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cat 101109_liquiditaet_mitgliedsbeitrag_shack.csv | ./liqicsv2memberscsv.pl|./fixliqicsvdates.pl >members.csv
+cat members.csv |./deltamemberscsv.pl >membersdelta.dat
+cat members.csv |./cummulativememberscsv.pl >memberscummulative.dat
+gnuplot members.plot
