943a80dc539b2634df694721ee89142d9da5982c
hdznrrd <hadez@nrrd.de>
2010-12-06 22:10:35 +0100

    added helper script to convert text pasted from PDF tables to csv

---
+++ gnuplot/shackspace/bookings2csv.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+#
+#
+# convert pasted txt from pdf to csv
+
+
+
+#15.04.2010 53/3 shack Snack-Bar              Sven Schoengarth, Moritz Fren-      16,00
+
+%data=();
+while($_=<STDIN>)
+{
+    chomp();
+    @tok = /^(\d+).(\d+).(\d+).+?(-?\d+,\d+)/;
+    $date = "$tok[2]-$tok[1]-$tok[0]";
+    $tok[3] =~ s/,/./g;
+    $data{$date} += $tok[3];
+}
+
+foreach(sort keys %data)
+{
+    print "$_ $data{$_}\n";
+}
