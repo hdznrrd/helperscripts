64777f7bbee98163bf2d8f40ea77cea90fe36266
hdznrrd <hadez@nrrd.de>
2010-12-06 22:46:09 +0100

    update for additional scripts

--- gnuplot/shackspace/process.sh
+++ gnuplot/shackspace/process.sh
@@ -3,5 +3,6 @@
 cat 101109_liquiditaet_mitgliedsbeitrag_shack.csv | ./liqicsv2memberscsv.pl|./fixliqicsvdates.pl >members.csv
 cat members.csv |./deltamemberscsv.pl >membersdelta.dat
 cat members.csv |./cummulatememberscsv.pl >memberscummulative.dat
-cat *txt |./bookings2csv.pl >bookings.dat
+cat *txt |./bookings2dat.pl >bookings.dat
+cat *txt |./cummulativebookings2dat.pl >bookingscummulative.dat
 ./replot.sh
